1cd71b5ba3c9e7cf47b3b5d809f16673c6829884
Conrad Ludgate <conrad.ludgate@truelayer.com>
2021-12-29 11:49:54 +0000

    repackage

---
+++ tensorgraph-sys/src/lib.rs
@@ -0,0 +1,18 @@
+#![allow(incomplete_features)]
+#![feature(
+    generic_associated_types,
+    allocator_api,
+    alloc_layout_extra,
+    nonnull_slice_from_raw_parts,
+    slice_ptr_len,
+    ptr_metadata,
+    maybe_uninit_slice,
+    generic_const_exprs
+)]
+
+
+pub mod blas;
+pub mod device;
+pub mod ptr;
+pub mod vec;
+pub mod tensor;
