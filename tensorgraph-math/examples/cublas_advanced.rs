3b3a4cdd674fc5cbb313a0f093addce65fb1ac68
Conrad Ludgate <conrad.ludgate@truelayer.com>
2022-01-03 20:31:47 +0000

    anoter example

--- tensorgraph-math/examples/cublas_advanced.rs
+++ tensorgraph-math/examples/cublas_advanced.rs
@@ -1,6 +1,6 @@
 use tensorgraph_sys::{
     device::cuda::{Context, Stream},
-    DefaultVec, View,
+    Vec, View,
 };
 
 use tensorgraph_math::{blas::cublas::CublasContext, tensor::Tensor};
@@ -9,18 +9,17 @@ fn main() {
     // init cuda context
     let cuda_ctx = Context::quick_init().unwrap();
 
-    // create cuda stream and configure it as the global
+    // create cuda stream
     let stream = Stream::new(&cuda_ctx).unwrap();
-    let _handle = stream.as_global();
 
-    // create cublas context, with the provided stream, and configure it as the global
+    // create cublas context, with the provided stream
     let cublas_ctx = CublasContext::new();
-    let _handle = cublas_ctx.with_stream(Some(&stream)).as_global();
+    let cublas_ctx = cublas_ctx.with_stream(Some(&stream));
 
-    run()
+    run(cublas_ctx, &stream);
 }
 
-fn run() {
+fn run(ctx: CublasContext, alloc: &Stream) {
     //     0 1
     // A = 2 3
     //     4 5
@@ -29,8 +28,8 @@ fn run() {
     //     2 3
 
     // column major (read each column first)
-    let a = DefaultVec::<f32, Cuda>::copy_from_host(&[0., 2., 4., 1., 3., 5.]);
-    let b = DefaultVec::<f32, Cuda>::copy_from_host(&[0., 2., 1., 3.]);
+    let a = Vec::copy_from_host_in(&[0., 2., 4., 1., 3., 5.0_f64], alloc);
+    let b = Vec::copy_from_host_in(&[0., 2., 1., 3.], alloc);
 
     let a = Tensor::from_shape([3, 2], a); // 3 rows x 2 cols
     let b = Tensor::from_shape([2, 2], b); // 2 rows x 2 cols
@@ -39,6 +38,6 @@ fn run() {
     // C = AB =  6 11
     //          10 19
 
-    let c = a.dot(b.view());
+    let c = a.dot_into(b.view(), ctx, alloc);
     assert_eq!(c.into_inner().into_std(), [2., 6., 10., 3., 11., 19.]);
 }
